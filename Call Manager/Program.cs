f9e65a45c27b67ae4662eedc9afce0222e3ca78a
Mike Fink <mfink92@gmail.com>
2016-02-11 17:31:37 -0800

    Merged with config-settings changes

--- Call Manager/Program.cs
+++ Call Manager/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿//using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +11,6 @@ namespace Call_Manager
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
